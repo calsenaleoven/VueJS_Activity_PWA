303642cb30ba7ccc494e604f897538645f39d928
flitzzzz <135428971+flitzy8@users.noreply.github.com>
2023-10-12 18:26:30 +0800

    TEMPLATE GAME NO DESIGN NOT WORKING YET

--- vuejs_activity_pwa/src/router/index.js
+++ vuejs_activity_pwa/src/router/index.js
@@ -1,19 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Game from '../views/Game.vue'
+import Hightech from '../views/Hightech.vue'
+
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Game',
+    component: Game
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/manage',
+    name: 'Hightech',
+    component: Hightech
+
   }
 ]
 
---
+++ vuejs_activity_pwa/src/views/Game.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Taas/>
+        <Sect/>
+    </div>
+</template>
+<script>
+
+import Sect from '@/Game/Sect.vue';
+import Taas from '@/Game/Taas.vue';
+
+export default
+{
+    name: 'Game',
+    components: 
+    {
+        Taas,
+        Sect
+    }
+}
+
+</script>
+<style>
+@import url(https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css);
+@import url(https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css);
+
+
+
+
+
+
+
+
+</style>
